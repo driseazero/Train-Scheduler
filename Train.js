53a1f04db56b735b5d9a905ecf0b993b2c5cdadb
Driscilla Carbente <driseazer0@gmail.com>
2017-09-08 23:51:36 -0400

    Add files via upload

---
+++ Train.js
@@ -0,0 +1,11 @@
+
+  
+  var config = {
+    apiKey: "AIzaSyC3TmsyLbxxNr0Br1ch1qKbK74LXXTR7xg",
+    authDomain: "train-scheduler-d902e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d902e.firebaseio.com",
+    projectId: "train-scheduler-d902e",
+    storageBucket: "train-scheduler-d902e.appspot.com",
+    messagingSenderId: "489260499264"
+  };
+  var database = firebase.initializeApp(config);
